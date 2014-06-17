b6a05250e79883ef755955dcebb7d4932ab8060a
memiks <contact@memiks.fr>
2014-06-17 10:31:43 +0200

    Change for loop to retrieve all messages and use a temporary variable.

--- src/main/java/org/imaptodtb/log/IMAP.java
+++ src/main/java/org/imaptodtb/log/IMAP.java
@@ -78,7 +78,7 @@ public class IMAP {
          */
         retourMsg.append("line 78\n");
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss Z yyyy");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzzz yyyy");
         Emails emails = emailService.getLastMessage();
 
         retourMsg.append("line 83\n");
