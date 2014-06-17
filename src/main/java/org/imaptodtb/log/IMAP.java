3f620e607de08305635f5f38c26019aed79c1644
memiks <contact@memiks.fr>
2014-06-17 08:39:07 +0200

    Verify only new message

--- src/main/java/org/imaptodtb/log/IMAP.java
+++ src/main/java/org/imaptodtb/log/IMAP.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.imaptodtb.log;
 
 // Import the Commons/Net classes
@@ -13,6 +12,7 @@ import java.util.*;
 import javax.mail.*;
 import javax.mail.Flags.Flag;
 import javax.mail.internet.*;
+import javax.mail.search.SentDateTerm;
 import org.imaptodtb.entity.Emails;
 import org.imaptodtb.service.IEmailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,101 +20,111 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service
 public class IMAP {
 
-   @Autowired
-   IEmailsService emailService;
-   
-  public void test() throws Exception {
+    @Autowired
+    IEmailsService emailService;
 
-  String host = "mymail.pprgroup.net";
-  String user = "preveclient@siege.red";
-  String password = "Laredoute2014";
-  
-  Date today = new Date();
+    public void test() throws Exception {
+
+        String host = "mymail.pprgroup.net";
+        String user = "preveclient@siege.red";
+        String password = "Laredoute2014";
+
+        Date today = new Date();
         Calendar cal = Calendar.getInstance();
         cal.setTime(today);
         cal.add(Calendar.HOUR, -24);
         Date yesterday = cal.getTime();
         System.out.print("Date = " + yesterday);
          // Getting now.
-        
-  
+
         IMAPFolder folder = null;
         Store store = null;
         Flag flag = null;
-  
-      // Get system properties
-  Properties props = System.getProperties();
-  props.setProperty("mail.store.protocol", "imaps");
 
-  Session session = Session.getDefaultInstance(props, null);
+        // Get system properties
+        Properties props = System.getProperties();
+        props.setProperty("mail.store.protocol", "imaps");
 
-  // Get a Store object that implements the specified protocol.
-  store = session.getStore("imaps");
+        Session session = Session.getDefaultInstance(props, null);
 
-  //Connect to the current host using the specified username and password.
-  store.connect(host, user, password);
+        // Get a Store object that implements the specified protocol.
+        store = session.getStore("imaps");
 
-  //Create a Folder object corresponding to the given name.
-  folder = (IMAPFolder) store.getFolder("Inbox/Commande");
+        //Connect to the current host using the specified username and password.
+        store.connect(host, user, password);
 
-  // Open the Folder.
-  folder.open(Folder.READ_ONLY);
-    int messageCount = folder.getMessageCount();
+        //Create a Folder object corresponding to the given name.
+        folder = (IMAPFolder) store.getFolder("Inbox/Commande");
 
-   System.out.println("Total Messages:- " + messageCount);
+        // Open the Folder.
+        folder.open(Folder.READ_ONLY);
 
-  // Get the messages from the server
-  Message[] messages = folder.getMessages();
+        /*
+         int messageCount = folder.getMessageCount();
+         System.out.println("Total Messages:- " + messageCount);
+         // Get the messages from the server
+         Message[] messages = folder.getMessages();
+         */
+        MailDateFormat dateFormat = new MailDateFormat();
+        Emails emails = emailService.getLastMessage();
 
-    Emails email = new Emails();
-  // Display message.
-  for (int i = messages.length-1 ; i > messages.length-100; i--) {
+        SentDateTerm sentDateTerm = new SentDateTerm(SentDateTerm.GT, dateFormat.parse(emails.getSendDate()));
+
+        Message[] messages = folder.search(sentDateTerm);
+        if (messages == null || messages.length <= 0) {
+            System.out.println("Total new Messages: " + messages.length);
+        } else {
+            System.out.println("No new Messages !");
+        }
+
+        Emails email = new Emails();
+        // Display message.
+        for (int i = messages.length - 1; i > messages.length - 100; i--) {
       //if (messages[i].getContent().toString().contains("20140850747407")) {
-  //System.out.println("------------ Message " + (i + 1) + " ------------");
-  String from = InternetAddress.toString(messages[i].getFrom());
-  if (from != null) {
-  email.setFrom(from);
-  }
-  String replyTo = InternetAddress.toString(messages[i].getReplyTo());
-  if (replyTo != null) {
-  email.setReplyTo(replyTo);
-  }
-  String to = InternetAddress.toString(messages[i].getRecipients(Message.RecipientType.TO));
-  if (to != null) {
-  email.setTo(to);
-  }
-  String cc = InternetAddress.toString(messages[i].getRecipients(Message.RecipientType.CC));
-  if (cc != null) {
-  email.setCc(cc);
-  }
-  String bcc = InternetAddress.toString(messages[i].getRecipients(Message.RecipientType.BCC));
-  if (bcc != null) {
-  email.setBcc(bcc);
-  }
-  String subject = messages[i].getSubject();
-  if (subject != null) {
-  email.setSubject(subject);
-  }
-  Date sent = messages[i].getSentDate();
-  if (sent != null) {
-  email.setSendDate(sent.toString());
-  }
-  String message = messages[i].getContent().toString();
-  if (message != null) {
-  email.setMessage(message);
-  }
-  Date received = messages[i].getReceivedDate();
-  if (received != null) {
-  email.setReceivedDate(received.toString());
-  }
-  
-  emailService.insertEmails(email);
-  
-  }
-  //}
-  folder.close(true);
-  store.close();
-  
-  
-  }
-} 
+            //System.out.println("------------ Message " + (i + 1) + " ------------");
+            String from = InternetAddress.toString(messages[i].getFrom());
+            if (from != null) {
+                email.setFrom(from);
+            }
+            String replyTo = InternetAddress.toString(messages[i].getReplyTo());
+            if (replyTo != null) {
+                email.setReplyTo(replyTo);
+            }
+            String to = InternetAddress.toString(messages[i].getRecipients(Message.RecipientType.TO));
+            if (to != null) {
+                email.setTo(to);
+            }
+            String cc = InternetAddress.toString(messages[i].getRecipients(Message.RecipientType.CC));
+            if (cc != null) {
+                email.setCc(cc);
+            }
+            String bcc = InternetAddress.toString(messages[i].getRecipients(Message.RecipientType.BCC));
+            if (bcc != null) {
+                email.setBcc(bcc);
+            }
+            String subject = messages[i].getSubject();
+            if (subject != null) {
+                email.setSubject(subject);
+            }
+            Date sent = messages[i].getSentDate();
+            if (sent != null) {
+                email.setSendDate(sent.toString());
+            }
+            String message = messages[i].getContent().toString();
+            if (message != null) {
+                email.setMessage(message);
+            }
+            Date received = messages[i].getReceivedDate();
+            if (received != null) {
+                email.setReceivedDate(received.toString());
+            }
+
+            emailService.insertEmails(email);
+
+        }
+        //}
+        folder.close(true);
+        store.close();
+
+    }
+}
